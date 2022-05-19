872f755fb88cbe7a045ffe2530a5fdbec9fb1b23
vuongicesmile <boide03@gmail.com>
2022-05-19 11:22:43 +0700

    coin

--- my-app/src/Components/AddCompany/index.js
+++ my-app/src/Components/AddCompany/index.js
@@ -1,9 +1,10 @@
 import react from "react";
 
 import React, { useState } from "react";
-import { Modal, Button, Row, Col, Input, Select } from "antd";
+import { Modal, Button, Row, Col, Input, Select, Tabs } from "antd";
 import { Typography } from "antd";
 import { Divider } from "antd";
+import "./Currency.scss";
 
 import { Switch } from "antd";
 
@@ -11,7 +12,10 @@ import prefix from "../../images/prefix.svg";
 import address from "../../images/address.svg";
 import phone1 from "../../images/phone1.svg";
 import email from "../../images/email.svg";
+import dola from "../../images/dola.svg";
+import vnd from "../../images/vnd.svg";
 import up from "../../images/up.svg";
+import check from "../../images/check.svg";
 import CloseIcon from "../../images/close.js";
 
 const { Text } = Typography;
@@ -35,6 +39,11 @@ function AddCompany() {
   function onChange(checked) {
     console.log(`switch to ${checked}`);
   }
+  const { TabPane } = Tabs;
+
+  function callback(key) {
+    console.log(key);
+  }
 
   return (
     <>
@@ -42,18 +51,13 @@ function AddCompany() {
         Open Modal
       </Button>
       <Modal
-
         title="Add Company"
         visible={popupVisible}
         onOk={handleOk}
         onCancel={handleCancel}
         bodyStyle={{ paddingTop: "0px" }}
         closable={true}
-        closeIcon={
-           
-                <image src={email} alt="" />
-              
-            }
+        closeIcon={<image src={email} alt="" />}
         width={648}
         footer={[
           <Col className="container" key={key}>
@@ -86,208 +90,130 @@ function AddCompany() {
           </Col>,
         ]}
       >
-        {/* full width srcreen */}
-        <Divider
-          orientation="left"
-          orientationMargin="0"
-          dashed={true}
-          plain
-          style={{ borderTopColor: "white" }}
-        >
-          Info <Text type="warning">*</Text>
-        </Divider>
-        <Col className="container">
+        <div className="container">
           <Col span={24}>
-            {/* <Row span={24}> Info <Text type="warning">*</Text></Row> */}
-
-            <Row gutter={[38, 16]} justify="space-between">
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">ID</label>
-                <Input
-                  suffix={<img src={prefix} alt=""></img>}
-                  type="email"
-                  className="htmlForm-control"
-                  id="examplehtmlFormControlInput1"
-                  placeholder="Code"
-                />
+            <p className="m-0 title-label">Popular currencies</p>
+          </Col>
+        </div>
+        <Row gutter={[8, 8]}>
+          <Col
+            xl={12}
+            xxl={12}
+            lg={12}
+            md={12}
+            sm={24}
+            xs={24}
+            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
+          >
+            <Row gutter={10}>
+              <Col className="d-flex align-items-center">
+                <img src={dola}></img>
               </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Address</label>
-                <Input
-                  suffix={<img src={address} alt=""></img>}
-                  type="text"
-                  className="htmlForm-control"
-                  id="examplehtmlFormControlInput1"
-                  placeholder="Code"
-                />
-              </Col>
-              {/* name */}
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Name</label>
-                <Input
-                  suffix={<img src={prefix}></img>}
-                  type="email"
-                  className="htmlForm-control"
-                  id="examplehtmlFormControlInput1"
-                  placeholder="Code"
-                />
-              </Col>
-              {/* Tax */}
-
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <Row gutter={11}>
-                  <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                    <label htmlFor="examplehtmlFormControlInput1">Tax</label>
-                    <Input
-                      suffix={<img src={prefix}></img>}
-                      type="email"
-                      className="htmlForm-control"
-                      id="examplehtmlFormControlInput1"
-                      placeholder="Code"
-                    />
-                  </Col>
-                  <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                    <label htmlFor="examplehtmlFormControlInput1">Phone</label>
-                    <Input
-                      suffix={<img src={phone1}></img>}
-                      type="email"
-                      className="htmlForm-control"
-                      id="examplehtmlFormControlInput1"
-                      placeholder="Code"
-                    />
-                  </Col>
-                </Row>
-              </Col>
-
-              {/* legal name */}
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Legal Name</label>
-                <Input
-                  suffix={<img src={prefix}></img>}
-                  type="email"
-                  className="htmlForm-control"
-                  id="examplehtmlFormControlInput1"
-                  placeholder="Legal Name"
-                />
-              </Col>
-              {/* email + bank account */}
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <Row gutter={11}>
-                  <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                    <label htmlFor="examplehtmlFormControlInput1">Email</label>
-                    <Input
-                      suffix={<img src={email}></img>}
-                      type="email"
-                      className="htmlForm-control"
-                      id="examplehtmlFormControlInput1"
-                    />
-                  </Col>
-                  <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                    <label htmlFor="examplehtmlFormControlInput1">
-                      Bank account
-                    </label>
-                    <Input
-                      type="email"
-                      className="htmlForm-control"
-                      id="examplehtmlFormControlInput1"
-                    />
-                  </Col>
-                </Row>
-              </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">
-                  Credit limit
-                </label>
-                <Input
-                  suffix={<img src={prefix}></img>}
-                  type="email"
-                  className="htmlForm-control"
-                  id="examplehtmlFormControlInput1"
-                  placeholder="Number"
-                />
-              </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Booker</label>
-                <Select
-                  style={{ width: "100%" }}
-                  size="default"
-                  suffixIcon={<img src={up}></img>}
-                />
-              </Col>
-
-              <Col span={24}>
-                <span>Account receivable (AR)</span>
-                <Switch defaultChecked onChange={onChange} />
+              <Col className="d-flex flex-column justify-content-around">
+                <span>United States Dollar</span>
+                <span className="currency-label">USD - $</span>
               </Col>
             </Row>
           </Col>
-
-          <hr></hr>
-        </Col>
-        <Divider
-          orientation="left"
-          orientationMargin="0"
-          dashed={true}
-          plain
-          style={{ borderTopColor: "white" }}
-        >
-          Statistics <Text type="warning">*</Text>
-        </Divider>
-        <Col className="container">
-          <Col span={24}>
-            <Row gutter={[38, 16]} justify="space-between">
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">
-                  Source code
-                </label>
-                <Select
-                  style={{ width: "100%" }}
-                  size="default"
-                  placeholder="Source code"
-                  suffixIcon={<img src={up}></img>}
-                />
+          <Col
+            xl={12}
+            xxl={12}
+            lg={12}
+            md={12}
+            sm={24}
+            xs={24}
+            className="d-flex justify-content-between p-3 cursor-pointer currency-content selected"
+          >
+            <Row gutter={10}>
+              <Col className="d-flex align-items-center">
+                <img src={vnd}></img>
               </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Type</label>
-                <Select
-                  style={{ width: "100%" }}
-                  size="default"
-                  placeholder="Source code"
-                  suffixIcon={<img src={up}></img>}
-                />
+              <Col className="d-flex flex-column justify-content-around">
+                <span>United States Dollar</span>
+                <span className="currency-label">USD - $</span>
               </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">
-                  Market segment
-                </label>
-                <Select
-                  style={{ width: "100%" }}
-                  size="default"
-                  placeholder="Market segment"
-                  suffixIcon={<img src={up}></img>}
-                />
+            </Row>
+            <Col>
+              <img src={check}></img>
+            </Col>
+          </Col>
+          <Col
+            xl={12}
+            xxl={12}
+            lg={12}
+            md={12}
+            sm={24}
+            xs={24}
+            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
+          >
+            <Row gutter={10}>
+              <Col className="d-flex align-items-center">
+                <img src={dola}></img>
               </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Branch</label>
-                <Select
-                  style={{ width: "100%" }}
-                  size="default"
-                  placeholder="Market segment"
-                  suffixIcon={<img src={up}></img>}
-                />
-              </Col>
-              <Col xl={12} xxl={12} lg={12} md={12} sm={24} xs={24}>
-                <label htmlFor="examplehtmlFormControlInput1">Industry</label>
-                <Select
-                  placeholder="Industry"
-                  style={{ width: "100%" }}
-                  size="default"
-                  suffixIcon={<img src={up}></img>}
-                />
+              <Col className="d-flex flex-column justify-content-around">
+                <span>United States Dollar</span>
+                <span className="currency-label">USD - $</span>
               </Col>
             </Row>
           </Col>
-        </Col>
+          <Col
+            xl={12}
+            xxl={12}
+            lg={12}
+            md={12}
+            sm={24}
+            xs={24}
+            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
+          >
+            <Row gutter={10}>
+              <Col className="d-flex align-items-center">
+                <img src={dola}></img>
+              </Col>
+              <Col className="d-flex flex-column justify-content-around">
+                <span>United States Dollar</span>
+                <span className="currency-label">USD - $</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            xl={12}
+            xxl={12}
+            lg={12}
+            md={12}
+            sm={24}
+            xs={24}
+            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
+          >
+            <Row gutter={10}>
+              <Col className="d-flex align-items-center">
+                <img src={dola}></img>
+              </Col>
+              <Col className="d-flex flex-column justify-content-around">
+                <span>United States Dollar</span>
+                <span className="currency-label">USD - $</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            xl={12}
+            xxl={12}
+            lg={12}
+            md={12}
+            sm={24}
+            xs={24}
+            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
+          >
+            <Row gutter={10}>
+              <Col className="d-flex align-items-center">
+                <img src={dola}></img>
+              </Col>
+              <Col className="d-flex flex-column justify-content-around">
+                <span>United States Dollar</span>
+                <span className="currency-label">USD - $</span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Modal>
     </>
   );
