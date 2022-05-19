10d8748210e6592ce11e83d90e118f6c2c721224
vuongicesmile <boide03@gmail.com>
2022-05-19 20:29:54 +0700

    done task

--- my-app/src/Components/AddCompany/index.js
+++ my-app/src/Components/AddCompany/index.js
@@ -1,6 +1,6 @@
 import react from "react";
 
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Modal, Button, Row, Col, Input, Select, Tabs } from "antd";
 import { Typography } from "antd";
 import { Divider } from "antd";
@@ -8,21 +8,89 @@ import "./Currency.scss";
 
 import { Switch } from "antd";
 
-import prefix from "../../images/prefix.svg";
-import address from "../../images/address.svg";
-import phone1 from "../../images/phone1.svg";
+import cambodia from "../../images/prefix.svg";
+import cny from "../../images/address.svg";
+import rub from "../../images/phone1.svg";
 import email from "../../images/email.svg";
 import dola from "../../images/dola.svg";
+import thaiBaht from "../../images/dola.svg";
 import vnd from "../../images/vnd.svg";
 import up from "../../images/up.svg";
 import check from "../../images/check.svg";
-import CloseIcon from "../../images/close.js";
+
 
 const { Text } = Typography;
 
 function AddCompany() {
-  const key = "Unstopable";
+  const key = "upstopable";
   const [popupVisible, setpopupVisible] = useState(false);
+  const [checkSelectedVisible, setcheckSelectedVisible] = useState(false);
+
+  const [listFlag, setListFlag] = useState([
+    {
+      id: 'vn-flag',
+      flag: vnd,
+      name: 'Vietnamese Dong',
+      label: 'VND - ₫',
+      className: 'currency-content p-2',
+    },
+    {
+      id: 'cny-flag',
+      flag: cny,
+      name: 'Chinese Yuan',
+      label: 'CNY - ¥',
+      className: 'currency-content p-2',
+    },
+    {
+      id: 'rub-flag',
+      flag: rub,
+      name: 'Russian Ruble',
+      label: 'RUB - ₽',
+      className: 'currency-content p-2',
+    },
+    {
+      id: 'dola-flag',
+      flag: dola,
+      name: 'United States Dollar',
+      label: 'USD - $',
+      className: 'currency-content p-2',
+    },
+    {
+      id: 'thaiBaht-flag',
+      flag: thaiBaht,
+      name: 'Thai Baht',
+      label: 'THB - ฿',
+      className: 'currency-content p-2',
+    },
+    {
+      id: 'cambodia-flag',
+      flag: cambodia,
+      name: 'Cambodian Riel',
+      label: 'Riel - ៛',
+      className: 'currency-content p-2',
+    },
+  ]);
+
+  const handleActive = (flag) => {
+    let activeItem = { id: '', flag: null, name: '', label: '', className: '' };
+    activeItem = { ...flag, className: 'currency-content p-2 selected' };
+
+    let newArray = [];
+    listFlag.map((item) => {
+      if (item.id == activeItem.id) {
+        item.className = 'currency-content p-2 selected';
+        newArray.push(item);
+      } else {
+        item.className = 'currency-content p-2';
+        newArray.push(item);
+      }
+    });
+    setListFlag(newArray);
+  };
+
+  useEffect(() => {
+    //console.log(listFlag);
+  }, [listFlag]);
 
   const showModal = () => {
     setpopupVisible(true);
@@ -36,14 +104,7 @@ function AddCompany() {
     setpopupVisible(false);
   };
 
-  function onChange(checked) {
-    console.log(`switch to ${checked}`);
-  }
-  const { TabPane } = Tabs;
 
-  function callback(key) {
-    console.log(key);
-  }
 
   return (
     <>
@@ -95,124 +156,44 @@ function AddCompany() {
             <p className="m-0 title-label">Popular currencies</p>
           </Col>
         </div>
-        <Row gutter={[8, 8]}>
-          <Col
-            xl={12}
-            xxl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
-          >
-            <Row gutter={10}>
-              <Col className="d-flex align-items-center">
-                <img src={dola}></img>
+        <Row gutter={[2, 2]}>
+          
+          {listFlag.map((item, index) => {
+            console.log(item.className);
+            console.log("re render");
+            return (
+              <Col
+                xl={12}
+                xxl={12}
+                lg={12}
+                md={12}
+                sm={24}
+                xs={24}
+                className="d-flex justify-content-between cursor-pointer"
+                key={item.id}
+              >
+                <div
+                  className={item.className}
+                  onClick={() => handleActive(item)}
+                >
+                  <Row gutter={10}>
+                    <Col className="d-flex align-items-center">
+                      <img id={item.id} src={item.flag} alt=""></img>
+                    </Col>
+                    <Col className="d-flex flex-column justify-content-around">
+                      <span>{item.name}</span>
+                      <span className="currency-label">{item.label}</span>
+                    </Col>
+                    {item.className == 'currency-content p-2 selected' && (
+                      <div className="currency-check">
+                        <img src={check}></img>
+                      </div>
+                    )}
+                  </Row>
+                </div>
               </Col>
-              <Col className="d-flex flex-column justify-content-around">
-                <span>United States Dollar</span>
-                <span className="currency-label">USD - $</span>
-              </Col>
-            </Row>
-          </Col>
-          <Col
-            xl={12}
-            xxl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-content-between p-3 cursor-pointer currency-content selected"
-          >
-            <Row gutter={10}>
-              <Col className="d-flex align-items-center">
-                <img src={vnd}></img>
-              </Col>
-              <Col className="d-flex flex-column justify-content-around">
-                <span>United States Dollar</span>
-                <span className="currency-label">USD - $</span>
-              </Col>
-            </Row>
-            <Col>
-              <img src={check}></img>
-            </Col>
-          </Col>
-          <Col
-            xl={12}
-            xxl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
-          >
-            <Row gutter={10}>
-              <Col className="d-flex align-items-center">
-                <img src={dola}></img>
-              </Col>
-              <Col className="d-flex flex-column justify-content-around">
-                <span>United States Dollar</span>
-                <span className="currency-label">USD - $</span>
-              </Col>
-            </Row>
-          </Col>
-          <Col
-            xl={12}
-            xxl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
-          >
-            <Row gutter={10}>
-              <Col className="d-flex align-items-center">
-                <img src={dola}></img>
-              </Col>
-              <Col className="d-flex flex-column justify-content-around">
-                <span>United States Dollar</span>
-                <span className="currency-label">USD - $</span>
-              </Col>
-            </Row>
-          </Col>
-          <Col
-            xl={12}
-            xxl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
-          >
-            <Row gutter={10}>
-              <Col className="d-flex align-items-center">
-                <img src={dola}></img>
-              </Col>
-              <Col className="d-flex flex-column justify-content-around">
-                <span>United States Dollar</span>
-                <span className="currency-label">USD - $</span>
-              </Col>
-            </Row>
-          </Col>
-          <Col
-            xl={12}
-            xxl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-content-start p-3 cursor-pointer currency-content"
-          >
-            <Row gutter={10}>
-              <Col className="d-flex align-items-center">
-                <img src={dola}></img>
-              </Col>
-              <Col className="d-flex flex-column justify-content-around">
-                <span>United States Dollar</span>
-                <span className="currency-label">USD - $</span>
-              </Col>
-            </Row>
-          </Col>
+            );
+          })}
         </Row>
       </Modal>
     </>
